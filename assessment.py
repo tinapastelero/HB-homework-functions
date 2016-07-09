0f7a8e01716bfae116f8cc115d5bed98f8ef1f57
Tina Pastelero <tinapastelero@gmail.com>
2016-07-09 16:22:39 +0000

    finished part 2

--- assessment.py
+++ assessment.py
@@ -40,24 +40,45 @@ def is_berry(fruit):
     fruit_list = ['strawberry','cherry','blackberry'] #define list of fruits to check for
     return fruit in fruit_list
 
-
 #    (b) Write another function, shipping_cost(), which calculates shipping cost
 #        by taking a fruit name as a string, calling the `is_berry()` function 
 #        within the `shipping_cost()` function and returns `0` if ``is_berry()
 #        == True``, and `5` if ``is_berry() == False``.
 
+def shipping_cost(fruit):
+    if is_berry(fruit) == True:
+        cost = 0
+    else:
+        cost = 5
+    return cost
+
+
 # 2. (a) Write a function, `is_hometown()`, which takes a town name as a string
 #        and evaluates to `True` if it is your hometown, and `False` otherwise.
 #
+
+def is_hometown(town_name):
+    return town_name == "Manila"
+
 #    (b) Write a function, `full_name()`, which takes a first and last name as
 #        arguments as strings and returns the concatenation of the two names in
 #        one string.
 #
+
+def full_name(first_name, last_name):
+    return "{} {}".format(first_name,last_name) 
+
 #    (c) Write a function, `hometown_greeting()`, which takes a home town, a
 #        first name, and a last name as strings as arguments, calls both
 #        `is_hometown()` and `full_name()` and prints "Hi, 'full name here',
 #        we're from the same place!", or "Hi 'full name here', where are you 
 #        from?" depending on what `is_hometown()` evaluates to.
+
+def hometown_greeting(town_name, first_name, last_name):
+    if is_hometown(town_name) == True:
+        return "Hi {}, we're from the same place!".format(full_name(first_name,last_name))
+    else:
+        return "Hi {}, where are you from?".format(full_name(first_name,last_name))
 
 #####################################################################
 
